a50c27efd92f6f70febb309d4c5f897eaab91f6a
joydipnath <joydipsworld@gmail.com>
2017-03-10 08:25:59 +0530

    admin auth

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminLoginController extends Controller
 {
@@ -15,11 +16,24 @@ class AdminLoginController extends Controller
 	}
 
     public function showLoginForm(){
-
     	return view('auth.admin-login');
     }
 
-    public function login(){
-    	return true;
+    public function login(Request $request){
+    	
+    	//validate the form data
+    	$this->validate($request,[
+    		'email'    => 'required|email',
+    		'password' => 'required|min:6'
+    	]);
+
+    	//Attemt to login the user in
+    	if(Auth::guard('admin')->attempt(['email'=> $request->email,'password' => $request->password], $request->remember)){
+    		//if successfull, then take to 
+    		return redirect()->intended(route('admin.dashboard'));
+    	}
+
+    	//if unsuccessfull, then take it back to login page
+    	return redirect()->back()->withInput($request->only('email','remember'));
     }
 }
