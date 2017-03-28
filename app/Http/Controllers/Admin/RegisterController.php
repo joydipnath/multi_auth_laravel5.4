f9aa5f3654c7d197225f441706ed77c059f85e69
joydipnath <joydipsworld@gmail.com>
2017-03-28 07:40:38 +0530

    vendor rm

--- app/Http/Controllers/Admin/RegisterController.php
+++ app/Http/Controllers/Admin/RegisterController.php
@@ -65,7 +65,8 @@ class RegisterController extends Controller
         $this->validator($request->all())->validate();
 
         DB::beginTransaction();
-        try{
+
+        try {
 
         event(new Registered($user = $this->create($request->all())));
 
--- app/role.php
+++ app/role.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+Use DB;
 
 class role extends Model
 {
-    //
+    
+	public function create(){
+		
+	}
+
 }
